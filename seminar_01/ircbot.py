156c92bbb015012cf89cc9be0abfdef05a75887e
Marek Osvald <drekorian@drekorian.cz>
2012-11-01 01:42:20 +0100

    Reconfigures documentation for first seminar
    Also minor refactoring of the custom text file name.

--- seminar_01/ircbot.py
+++ seminar_01/ircbot.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
+
+"""
+Hello what does this do?.
+"""
+
 import re
 import state
 
-KARMA = {}
+KARMA = {} #: Global dictionary for storing users' karma
 
 def cmd_shutdown(msg):
 	"""
 	Raises SystemExit exception.
 
+	@param msg: message associated with the command
 	@return: None
 	@rtype: None
+	@raise SystemException: every time
 	"""
 	raise SystemExit
 
 def cmd_calc(exp):
 	"""
-	Calculates mathematical expression.
+	Calculates mathematical expression and returns a state with the result.
 
+	@param exp: expression to be calculated
 	@return: result of the expression
 	@rtype: state
 	"""
@@ -31,29 +39,62 @@ def cmd_calc(exp):
 	return state.next(exp) # exp contains non-mathematical symbols
 
 def cmd_word_count(msg):
+	"""
+	Returns the number of received words so far.
+
+	@param msg: message associated with the command
+	@return: "state withe the message 'Actual word count is <number> words'
+	@rtype: str
+	"""
 	return state.done("Actual word count is " + str(WORDCOUNT) + " words.")
 
-def cmd_karma(msg):
-	points = KARMA.get(msg, 0)
+def cmd_karma(user):
+	"""
+	Returns a state with number of karma points for given user.
 
-	return state.done("'" + msg + "' has " + str(points) + " points of karma")
+	@param user: user whose karma is queried
+	@return: done state with message '<user> has <number> points of karma'"
+	@rtype: state
+	"""
+	points = KARMA.get(user, 0)
+
+	return state.done("'" + user + "' has " + str(points) + " points of karma")
+
 
 COMMANDS = {
 	"SHUTDOWN":   cmd_shutdown,
 	"calc":       cmd_calc,
 	"word-count": cmd_word_count,
 	"karma":      cmd_karma,
-}
+} #: Global dictionary for storing commands
 
 
-WORDCOUNT = 0
+WORDCOUNT = 0 #: Global variable for storing the number of received words
 
 def f_word_count(msg):
+	"""
+	Filter that adds the number of words in processed message.
+
+	@param msg: message to be parsed
+	@return next state with the original message
+	@rtype: state
+	"""
 	global WORDCOUNT
 	WORDCOUNT += len(msg.split(" "))
 	return state.next(msg)
 
 def f_karma(msg):
+	"""
+	Checks the message for sequence 'something++' and 'something--', if found,
+	it calls the change_karma function. Depending whether the command is valid,
+	returns either done state or next state with appropriate message.
+
+	@return: done state if the information is done parsing
+	@rtype: state
+	@raise Exception: in case that change_karma return other than done or next
+	state
+	"""
+
 	pattern = "\S+(\+{2}|\-{2})"
 	if re.match(pattern, msg):
 		length = len(msg)
@@ -70,27 +111,31 @@ def f_karma(msg):
 	else:
 		return state.next(msg)
 
-FILTERS = [f_word_count, f_karma]
+FILTERS = [f_word_count, f_karma] #: Global array for storing the filters
 
-def change_karma(msg, inc = True):
+def change_karma(msg, increase = True):
+	"""
+	Changes users karma by one.
+	"""
+
 	try:
-		user, action = msg.split(" ", 1)
+		msg, action = msg.split(" ", 1)
 	except ValueError:
 		return state.next(msg)
 
-	KARMA.setdefault(user, 0)
+	KARMA.setdefault(msg, 0)
 
 	if action == "++":
-		KARMA[user] += 1
-		log = user + "'s karma was increased by 1."
+		KARMA[msg] += 1
+		log = msg + "'s karma was increased by 1."
 	elif action == "--":
-		KARMA[user] -= 1
-		log = user + "'s karma was decreased by 1."
+		KARMA[msg] -= 1
+		log = msg + "'s karma was decreased by 1."
 	else:
 		return state.next(msg)
 
-	if KARMA[user] == 0:
-		del KARMA[user]
+	if KARMA[msg] == 0:
+		del KARMA[msg]
 
 	return state.done(log)
 
