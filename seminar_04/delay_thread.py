897d3b9a8ad064d1a836e130c771f3c362428d4d
Marek Osvald <drekorian@drekorian.cz>
2012-12-17 04:08:54 +0100

    Adds homework assigned in seminar 04.
    Implements delay_thread.py and url_downloader.py for asynchronous
    operations of the bot.

---
+++ seminar_04/delay_thread.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+"""
+This module contains a class that represents a thread for delaying messages.
+
+@author: Marek Osvald
+@version: 2012.1216
+@since: 2012.1105
+
+@undocumented: __package__
+"""
+
+from plugins import state
+import time
+import threading
+import Queue
+
+class DelayThread(threading.Thread):
+	"""
+	Class representing a thread that delay received message by a few seconds.
+	"""
+	def __init__(self, interface, delay = 5):
+		"""
+		Parametric constructor.
+
+		@param interface: interface to use for writing the messages
+		@type interface: interface.IrcBotInterface
+		@param delay: delay time in seconds, default is 5
+		@type delay: int
+		"""
+		threading.Thread.__init__(self)
+
+		self._interface = interface
+		self._delay = delay
+		self._running = True
+		self._queue = Queue.Queue()
+
+	def run(self):
+		"""
+		While the thread is designated as running, runs an endless loop while
+		taking the messages from the queue and writing them via the interface.
+		@return: None
+		@rtype: None
+		"""
+		while self._running:
+			if not self._queue.empty():
+				msg = self._queue.get()
+				print msg
+				time.sleep(self._delay)
+				self._interface.write(msg)
+
+	def stop(self):
+		"""
+		Designates the thread as not running.
+
+		@return: None
+		@rtype: None
+		"""
+		self._running = False
+
+	def put_msg(self, msg):
+		"""
+		Adds a message to the queue.
+
+		@param msg: message to enqueue
+		@type msg: str
+		@return: done statement with received message
+		@rtype: state.done
+		"""
+		self._queue.put(msg)
+		return state.done() # returns none in order to force delay
---
+++ seminar_04/ircbot.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+
+"""
+This module is a mockup of a IRC bot. When executed, it runs an endless loop
+that can be interrupted with EOF keystroke or interrupt keystroke. Bot is
+capable of handling commands and filters.
+
+@author: Marek Osvald
+@version: 2012.1214
+@since: 2012.0510
+
+@undocumented: __package__
+"""
+from plugins import state
+import sys
+import plugins
+import interface
+import delay_thread
+import url_thread
+
+class IrcBot(object):
+	"""
+	Class representing the bot with a local shell interface.
+	"""
+	COMMANDS = plugins.COMMANDS #: A dictionary of commands processed by bot.
+	FILTERS = plugins.FILTERS #: An array of filters processed by bot.
+
+	def __init__(self, interface):
+		"""
+		Parametric constructor. Specifies an interface for the bot.
+
+		@param interface: implementation of the I/O interface
+		@type interface: object
+		"""
+		self._if = interface
+
+	def parse(self, msg):
+		"""
+		Parses given input.
+
+		@param msg: input message to parse
+		@type msg: str
+		@return: return message processed with associated commands and filters
+		@rtype: str
+		"""
+		try:
+			cmd, args = msg.split(" ", 1)
+		except ValueError:
+			cmd, args = msg.rstrip(), ""
+
+		if cmd in self.COMMANDS:
+			ret = self.COMMANDS[cmd](args)
+
+			if state.is_done(ret):
+				return ret.value
+			elif state.is_next(ret):
+				pass
+			else:
+				raise Exception("Illegal state of command.")
+
+		for filter in self.FILTERS:
+			ret = filter(msg)
+
+			if state.is_done(ret):
+				return ret.value
+
+			if state.is_replace(ret):
+				msg = ret.value
+
+		return msg
+
+	def run(self):
+		"""
+		Runs an endless loop that creates the gist of the bot.
+		"""
+		while True:
+			msg = self._if.read()
+			msg = self.parse(msg)
+			if msg:
+				print(msg)
+				self._if.write(msg)
+
+
+if __name__ == "__main__":
+	ifc = interface.IRCBotTelnetInterface(address = "localhost", port = 8082)
+	ifc.open()
+	bot = IrcBot(ifc)
+
+	delay = delay_thread.DelayThread(ifc)
+	url = url_thread.URLThread(ifc)
+	bot.COMMANDS["delay"] = delay.put_msg
+	bot.FILTERS.append(url.enqueue)
+
+	try:
+		delay.start()
+		url.start()
+		bot.run()
+	except (KeyboardInterrupt, EOFError, SystemExit):
+		print "Shutting down the bot"
+		delay.stop()
+		url.stop()
+		ifc.close()
+		sys.exit(0)
